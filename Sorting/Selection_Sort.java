0d85237bd0254e3f79857920f9fd2c433b094e48
Sanika Faske <143709223+Sanika-Faske@users.noreply.github.com>
2024-08-17 12:16:39 +0530

    Add files via upload

---
+++ Sorting/Selection_Sort.java
@@ -0,0 +1,31 @@
+package Sorting;
+
+public class Selection_Sort {
+
+	public static void printArray(int[] arr) {
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] arr= {7,8,3,1,2};
+		
+		//Time Complexity = O(n^2)
+		//Selection Sort
+		for(int i=0; i<arr.length-1; i++) {
+			int smallest=i;
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[j] < arr[smallest]) {
+					smallest=j;
+				}
+			}
+			int temp=arr[smallest];
+			arr[smallest]=arr[i];
+			arr[i]=temp;
+		}
+		
+		printArray(arr);
+	}
+
+}
